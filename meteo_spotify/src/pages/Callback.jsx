d61c53792a9a1397d1f322cef33b37dd0c5b0750
fab85270 <coutanceauf85@gmail.com>
2021-10-12 23:50:44 +0200

    ee

--- meteo_spotify/src/pages/Callback.jsx
+++ meteo_spotify/src/pages/Callback.jsx
@@ -1,9 +1,11 @@
 const Callback = () =>{
     return(
-    <>  
-        
-        <h1>Bienvenu dans le callback de l'application</h1>     
-    </>
+         
+        <h1><strong>Bienvenu dans le callback de l'application</strong></h1>     
+        /* Dans ce callback, récupérer le token depuis le access_token de l'URL. 
+            -> faire des boutons pour saisir chacune des fonctionalités proposées par cette application : lire musique.. proposer playlist selon le temps..
+            -> faire accès a l'API météo.
+            -> voir autre possibilités. */
     );
 }
 
