66f04c0e4d6a715e659b853a24156bc3231e79b2
fab85270 <coutanceauf85@gmail.com>
2021-10-13 13:54:04 +0200

    ee

---
+++ meteo_spotify/src/Layout/LayoutGlobal.jsx
@@ -0,0 +1,14 @@
+import { Children } from "react";
+import Navbar from '../components/Navbar/Navbar';
+
+const LayoutGlobal= ({children}) =>{
+    return(
+        <div>
+            <Navbar></Navbar> 
+            {children} 
+        </div> 
+        /* Ajouter un footer (bas de page) */ 
+    );
+}
+
+export default LayoutGlobal;
--- meteo_spotify/src/pages/About.jsx
+++ meteo_spotify/src/pages/About.jsx
@@ -1,14 +1,16 @@
 
 import Navbar from '../components/Navbar/Navbar';
-
+import LayoutGlobal from '../Layout/LayoutGlobal';
 /* Demander comment utiliser le navbar dans toute les "classes" ? le faire hériter ? utiliser "contexte" ?? */
 const About = () =>{
     return(
-    <>  
-        <Navbar></Navbar>
-        <h1>About</h1>     
-    </>
+    <LayoutGlobal children={
+        <h1>About</h1>  
+    }>         
+    </LayoutGlobal>
     );
 }
 
 export default About;
+
+/* Crée un layout assez global avec une navbar ou autre élément présent sur chaque page qui est toujours présente et modifier  */
