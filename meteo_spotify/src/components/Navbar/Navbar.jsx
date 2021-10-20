4714902b3fcc0864a971951913874e31d27c6579
fab85270 <coutanceauf85@gmail.com>
2021-10-20 14:38:14 +0200

    ee

--- meteo_spotify/src/components/Navbar/Navbar.jsx
+++ meteo_spotify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, {useState } from 'react';
 import AccessTokenContextProvider from '../../pages/Context/AccessTokenContext';
 import './Navbar.css';
 
