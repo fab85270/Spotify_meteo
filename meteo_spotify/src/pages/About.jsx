2270c8d522bc9e65fe343d498b1563106180ec3b
fab85270 <coutanceauf85@gmail.com>
2021-10-20 16:52:26 +0200

    ee

--- meteo_spotify/src/pages/About.jsx
+++ meteo_spotify/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState } from 'react';
+import React, {useContext} from 'react';
 import LayoutGlobal from '../Layout/LayoutGlobal';
 import {AccessTokenContext} from '../Context/AccessTokenContext';
 
