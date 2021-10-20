5d2fedc9f20749b136a3b1df7f6ad73b72614a62
fab85270 <coutanceauf85@gmail.com>
2021-10-20 14:23:00 +0200

    e

--- meteo_spotify/src/App.js
+++ meteo_spotify/src/App.js
@@ -7,22 +7,26 @@ import About from './pages/About';
 import Callback from './pages/Callback';
 
 
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <SpotiTherLayout/>
-        </Route>
-        <Route path="/about">
-          <About/>
-        </Route>
-        <Route path="/callback">
-          <Callback/>
-        </Route>
-      </Switch>
-   
-    </Router>
+    <AccessTokenContextProvider>
+      <AccessTokenContext.Provider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <SpotiTherLayout/>
+            </Route>
+            <Route path="/about">
+              <About/>
+            </Route>
+            <Route path="/callback">
+              <Callback/>
+            </Route>
+          </Switch>
+        </Router>
+      </AccessTokenContext.Provider>
+    </AccessTokenContextProvider>
   );
 }
 
