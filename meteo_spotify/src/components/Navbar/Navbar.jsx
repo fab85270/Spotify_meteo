5904707963848b60871c2189caa91b972ad5e7af
fab85270 <coutanceauf85@gmail.com>
2021-10-23 13:37:24 +0200

    ee

--- meteo_spotify/src/components/Navbar/Navbar.jsx
+++ meteo_spotify/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import { useHistory,Link } from "react-router-dom";
 import {AccessTokenContext} from '../../Context/AccessTokenContext';
 import { BoutonContext } from '../../Context/BoutonContext';
 import { TraductionContext } from '../../Context/TraductionContext';
-import Dropdown from 'react-dropdown';
+import {Dropdown} from 'react-dropdown';
+import { Button,  ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
+
+
 
 import './Navbar.css';
 
@@ -63,24 +66,23 @@ const NavBar = () => {
                         </button>
                     </li>
                     <li>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-default">Menu déroulant</button>
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">    
-                                <span class="caret"></span>
-                                <span class="sr-only">Toggle Dropdown</span>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a href="#" title="Lien 1">Lien 1</a></li>
-                                <li><a href="#" title="Lien 2">Lien 2</a></li>
-                                <li><a href="#" title="Lien 3">Lien 3</a></li>
-
-                            </ul>
                                 <button onClick={() => traductionApp()} className="ApiSpotify">
                                     {traduction && "Translate"}
                                     {!traduction && "Traduire"}
                                 </button>
-                        </div>
                     </li>
+                
+                    <div>
+                
+                    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                        <li>
+                            <button>coucou</button>
+                            <button>ca va </button>
+  
+                        </li>
+                    </DropdownButton>
+                     </div>
+                  
                 </ul>
             </nav>
         </div>
