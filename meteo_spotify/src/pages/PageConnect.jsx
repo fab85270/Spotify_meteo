c57f2449e47e22e967a11b1db84ab04e0c3a2b10
fab85270 <coutanceauf85@gmail.com>
2021-10-27 14:06:07 +0200

    e

---
+++ meteo_spotify/src/pages/PageConnect.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LayoutGlobal from '../../Layout/LayoutGlobal';
+
+const PageConnect = () => {
+    return(   
+        <LayoutGlobal children={   
+    
+            <>  
+                <h1>About</h1> 
+            </>
+        }>         
+        </LayoutGlobal>
+        );
+}
+export default PageConnect;
