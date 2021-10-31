fa1eaa1795af3f92bcdf0029060f25ada8f390f6
fab85270 <coutanceauf85@gmail.com>
2021-10-31 17:04:09 +0100

    ee

--- meteo_spotify/src/components/Form_research/Form_research.jsx
+++ meteo_spotify/src/components/Form_research/Form_research.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 const SearchForm = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -13,12 +14,9 @@ const SearchForm = (props) => {
   const handleSearch = (event) => {
     event.preventDefault();
 
-    if (searchTerm.trim() !== '') {
-      setErrorMsg('');
-      props.handleSearch(searchTerm);
-    } else {
-      setErrorMsg('Please enter a search term.');
-    }
+    /* Connexion à l'API Spotify à partir */
+
+
   };
 
   return (
