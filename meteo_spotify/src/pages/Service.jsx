9ff07dea4c05fb586deb73d1ac29af54143873c2
fab85270 <coutanceauf85@gmail.com>
2021-11-02 15:52:41 +0100

    ee

--- meteo_spotify/src/pages/Service.jsx
+++ meteo_spotify/src/pages/Service.jsx
@@ -1,10 +1,8 @@
 
 import LayoutGlobal from '../Layout/LayoutGlobal';
-import {AccessTokenContext} from '../Context/AccessTokenContext';
 import React, {useContext} from 'react';
 import { TraductionContext } from '../Context/TraductionContext';
-import ButtonRedirection from '../components/Button/ButtonRedirection';
-import { useHistory,Link } from "react-router-dom";
+
 
 const Service = () =>{
     return(
