ab9747c4a03d6ed6a3e2e388e9e02829d51a9c2c
fab85270 <coutanceauf85@gmail.com>
2021-11-03 11:20:18 +0100

    ee

--- meteo_spotify/src/Context/TraductionContext.js
+++ meteo_spotify/src/Context/TraductionContext.js
@@ -15,7 +15,7 @@ export const TraductionContextProvider = ({children}) => {
     
     const traductionApp = (event) => {
         /* La traduction ne sera possible que si on click sur Anglais ou Français */
-        if(event.target.value == 1 || event.target.value == 2){
+        if(event.target.value == 1 && !traduction || event.target.value == 2 && traduction){
             setTraduction(!traduction);
         }
     }
