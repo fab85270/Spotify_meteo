8ec11b62f5678341e5bbecea8e426917e82de224
fab85270 <coutanceauf85@gmail.com>
2021-11-03 16:22:21 +0100

    ee

--- meteo_spotify/src/components/SpotiTherLayout/SpotiTherLayout.jsx
+++ meteo_spotify/src/components/SpotiTherLayout/SpotiTherLayout.jsx
@@ -1,21 +1,13 @@
 
-//Ici on devra instaurer dans notre composant tout les autres composants déja attribués et dans le index.js, changer le App par CatFactLayout 
-/* qui permettra de ne pas avoir l'interface de base de react mais ici notre composant CatFactLayout en premier. 
-la fonction "https://catfact.ninja/fact " permettra de générer des textes aléatoires
-
-/!\  Un état est atteignable dans tout un composant. Donc il est atteignable depuis toutes les fcts  
-/!\   Connection a l'API se fait toujours dans le layout
-*/
-
-
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ajouter le boostrap au sein de l'application
 import LayoutGlobal from '../../Layout/LayoutGlobal';
 import ButtonRedirection from '../Button/ButtonRedirection';
 import React, {useContext} from 'react';
 import {AccessTokenContext} from '../../Context/AccessTokenContext';
-import Carousel from 'react-bootstrap/Carousel' 
-import imageHome from './ImageHomePage/menu1.jpg';
+import Carousel from 'react-bootstrap/Carousel'; 
+import imageHome from './ImageHomePage/menu2.jpeg';
+import image2 from './ImageHomePage/imageHome.jpeg';
 import { TraductionContext } from '../../Context/TraductionContext';
 
 function SpotiTherLayout({}){ 
@@ -52,7 +44,7 @@ function SpotiTherLayout({}){
                         <Carousel.Item>
                             <img 
                                 className="d-block w-100"
-                                src=""
+                                src={image2}
                                 alt="e"
                             />
                             <Carousel.Caption>
