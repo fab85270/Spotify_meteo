c42fc0ebed3e5fd3697e987f74963b4cd03970cc
fab85270 <coutanceauf85@gmail.com>
2021-11-08 22:52:57 +0100

    e

--- meteo_spotify/src/Context/MeteoContext.js
+++ meteo_spotify/src/Context/MeteoContext.js
@@ -24,7 +24,7 @@ import React,{createContext, useState} from 'react'
 export const MeteoContextProvider = ({children}) => { //Ici le children va représenter tous les composants/fichiers/routes impactées par 
     //Ce context => il correspond a tous les enfants, ce qu'il y a entre les balises de AccessTokenContextProvider de App.js
   
-    /* Initialisation du context utilisé par le bouléen false */
+    /* Utilisation du hook (context,états) */
     const [codePostal,setCodePostal] = useState("");
     const [nomVille,setNomVille] = useState("");
     const [numTemps,setNumTemps] = useState(-1);
