61b62778e55b039cbe1f1a707e51782021458ee5
fab85270 <coutanceauf85@gmail.com>
2021-11-12 17:06:14 +0100

    ee

--- meteo_spotify/src/components/DisplayMeteo/DisplayMeteo.jsx
+++ meteo_spotify/src/components/DisplayMeteo/DisplayMeteo.jsx
@@ -1,5 +1,4 @@
 import React, {useContext, useState } from 'react';
-import Image from 'react-bootstrap/Image'
 import {MeteoContext} from '../../Context/MeteoContext';
 
 import tempsNuageux from '../../Images/img_meteo/nuageux.jpg';
