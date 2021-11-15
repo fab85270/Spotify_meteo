6149f0f42a9c4cf8441bf64ab7cdfd4c206cf015
fab85270 <coutanceauf85@gmail.com>
2021-11-15 23:56:03 +0100

    ee

--- meteo_spotify/src/pages/DisconnectPage.jsx
+++ meteo_spotify/src/pages/DisconnectPage.jsx
@@ -1,4 +1,5 @@
 import Alert from 'react-bootstrap/Alert'
+import {TraductionContext } from '../Context/TraductionContext';
 import React, {useContext} from 'react';
 import Button from 'react-bootstrap/Button'
 import { useHistory} from "react-router-dom";
@@ -6,22 +7,24 @@ import { useHistory} from "react-router-dom";
 
 const DisconnectPage = () => {
 
+    const {traduction,traductionApp} = useContext(TraductionContext);
     let history = useHistory();
+    console.log("La traduction est de : "+traduction);
 
     return (
         <Alert variant="warning"  dismissible>
           <Alert.Heading>WARNING</Alert.Heading>
           <p>
-
-              Votre session vient d'être expirée. Veuillez vous enregistrer de nouveau
+            {traduction && "Your session has expired. Please log in again"}
+            {!traduction && "Votre session vient d'être expirée. Veuillez vous enregistrer de nouveau"}
           </p>
           <Button variant="danger" onClick={() => history.push('/')}>
-              Quitter
-
+              {traduction && "Cancel"}
+              {!traduction && "Quitter"}
           </Button>
           <Button variant="success" onClick={() => history.push('/connectAPI')}>
-            Connecter
-
+              {traduction && "Connect"}
+              {!traduction && "Se connecter"}
           </Button>
         </Alert>
       );
